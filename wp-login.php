a53760d25a8f73d0d0095030cff682fa6fba073b
annthuy <annthuy212002@gmail.com>
2021-09-26 16:14:14 +0700

    1

--- wp-login.php
+++ wp-login.php
@@ -5,17 +5,17 @@
 	<title>Đăng nhập &lsaquo; Hoa Mọc Trên Cung Trăng &#8212; WordPress</title>
 	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
 <link rel="dns-prefetch" href="//s.w.org">
-<link rel="stylesheet" id="dashicons-css" href="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="https://annthuy.github.io/nhuthuy_wordpress/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://annthuy.github.io/nhuthuy_wordpress/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="https://annthuy.github.io/nhuthuy_wordpress/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="dashicons-css" href="https://annthuy.github.io/annthuy_wordpress/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="https://annthuy.github.io/annthuy_wordpress/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="https://annthuy.github.io/annthuy_wordpress/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="https://annthuy.github.io/annthuy_wordpress/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="https://annthuy.github.io/annthuy_wordpress/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
-	<link rel="icon" href="https://annthuy.github.io/nhuthuy_wordpress/wp-content/uploads/2021/09/R.gif" sizes="32x32">
-<link rel="icon" href="https://annthuy.github.io/nhuthuy_wordpress/wp-content/uploads/2021/09/R.gif" sizes="192x192">
-<link rel="apple-touch-icon" href="https://annthuy.github.io/nhuthuy_wordpress/wp-content/uploads/2021/09/R.gif">
-<meta name="msapplication-TileImage" content="https://annthuy.github.io/nhuthuy_wordpress/wp-content/uploads/2021/09/R.gif">
+	<link rel="icon" href="https://annthuy.github.io/annthuy_wordpress/wp-content/uploads/2021/09/R.gif" sizes="32x32">
+<link rel="icon" href="https://annthuy.github.io/annthuy_wordpress/wp-content/uploads/2021/09/R.gif" sizes="192x192">
+<link rel="apple-touch-icon" href="https://annthuy.github.io/annthuy_wordpress/wp-content/uploads/2021/09/R.gif">
+<meta name="msapplication-TileImage" content="https://annthuy.github.io/annthuy_wordpress/wp-content/uploads/2021/09/R.gif">
 	</head>
 	<body class="login no-js login-action-login wp-core-ui  locale-vi">
 	<script type="text/javascript">
@@ -24,7 +24,7 @@
 		<div id="login">
 		<h1><a href="https://vi.wordpress.org/">Xây dựng bằng WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="https://annthuy.github.io/nhuthuy_wordpress/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="https://annthuy.github.io/annthuy_wordpress/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Tên người dùng hoặc Địa chỉ Email</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -42,33 +42,33 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Tự động đăng nhập</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Đăng nhập">
-									<input type="hidden" name="redirect_to" value="https://annthuy.github.io/nhuthuy_wordpress/wp-admin/">
+									<input type="hidden" name="redirect_to" value="https://annthuy.github.io/annthuy_wordpress/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://annthuy.github.io/nhuthuy_wordpress/tai-khoan/lost-password/">Bạn quên mật khẩu?</a>
+								<a href="https://annthuy.github.io/annthuy_wordpress/tai-khoan/lost-password/">Bạn quên mật khẩu?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://annthuy.github.io/nhuthuy_wordpress/">&larr; Quay lại Hoa Mọc Trên Cung Trăng</a>		</p>
+			<a href="https://annthuy.github.io/annthuy_wordpress/">&larr; Quay lại Hoa Mọc Trên Cung Trăng</a>		</p>
 			</div>
-	<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+	<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="zxcvbn-async-js-extra">
 /*  */
-var _zxcvbnSettings = {"src":"https:\/\/annthuy.github.io\/nhuthuy_wordpress\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"https:\/\/annthuy.github.io\/annthuy_wordpress\/wp-includes\/js\/zxcvbn.min.js"};
 /*  */
 </script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -84,14 +84,14 @@ var pwsL10n = {"unknown":"Mật khẩu mạnh không xác định","short":"Rấ
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-08-02 03:13:39+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s đã ngừng hoạt động từ phiên bản %2$s! Sử dụng thay thế bằng %3$s."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /*  */
 var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};
 /*  */
 </script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
 var userProfileL10n = {"user_id":"0","nonce":"6161c90dc4"};
@@ -104,7 +104,7 @@ var userProfileL10n = {"user_id":"0","nonce":"6161c90dc4"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", {"translation-revision-date":"2021-08-02 03:13:39+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=1; plural=0;","lang":"vi_VN"},"Your new password has not been saved.":["Mật khẩu mới của bạn chưa được lưu."],"Hide":["Ẩn đi"],"Show":["Hiển thị"],"Confirm use of weak password":["Chấp nhận sử dụng mật khẩu yếu."],"Hide password":["Ẩn mật khẩu"],"Show password":["Hiện mật khẩu"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
-<script type="text/javascript" src="https://annthuy.github.io/nhuthuy_wordpress/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
+<script type="text/javascript" src="https://annthuy.github.io/annthuy_wordpress/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
